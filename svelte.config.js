6e7c9662121fc8cab9433a31cfe314dda6176cce
tintinrevient <zhaobenben007@googlemail.com>
2025-02-09 22:46:55 +0100

    update

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.env.NODE_ENV === "production" ? process.env.BASE_PATH : "",
 		},
 	}
 };
