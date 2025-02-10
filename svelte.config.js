cb796607ba879acf4b770c2f7a28a747078b070f
tintinrevient <zhaobenben007@googlemail.com>
2025-02-10 17:32:57 +0100

    update

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ const config = {
   kit: {
     adapter: adapter(),
     paths: {
-      base: process.env.NODE_ENV === "production" ? "/octopusar" : "",
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
     },
   },
 };
