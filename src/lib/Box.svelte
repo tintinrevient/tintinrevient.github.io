c05eafcca025db3c34f010d68406c21ea5a9fd72
tintinrevient <zhaobenben007@googlemail.com>
2025-05-10 14:29:43 +0200

    update

--- src/lib/Box.svelte
+++ src/lib/Box.svelte
@@ -5,6 +5,14 @@
     let left = 700 + round * 20;
     let top = 200 + round * 20;
 
+    let compRef;
+
+    let editable = false;
+
+    function handleDblClick(e) {
+        editable = true;
+    }
+
     function onMouseMove(e) {
         if (moving) {
             left += e.movementX;
@@ -37,16 +45,41 @@
 
 <svelte:window on:mousemove={onMouseMove} on:mouseup={onMouseUp} />
 
-<div use:dragAction style="left: {left}px; top: {top}px; overflow-y: scroll;" on:mouseover={mouseover} on:mouseleave={mouseleave} class="w-screen h-screen bg-yellow-200 shadow-md cursor-pointer hover:shadow-lg hover:shadow-gray-400">
-    <slot />
+<div class="boxcontainer" use:dragAction style="left: {left}px; top: {top}px;" on:mouseover={mouseover} on:mouseleave={mouseleave} bind:this={compRef}>
+    <div class="note w-screen h-screen bg-yellow-200 
+                shadow-md cursor-pointer 
+                hover:shadow-lg hover:shadow-gray-400"
+                on:dblclick={handleDblClick} 
+                contenteditable={editable}>
+        <slot />
+    </div>
+    <button class="x bg-red-200 text-white hover:bg-red-300" on:click={() => compRef.parentNode.removeChild(compRef)}>
+        X
+    </button>
 </div>
 
-
 <style>
-    div {
+    .boxcontainer {
         width: 163px;
         height: 163px;
         padding: 10px 10px 10px 10px;
         position: absolute;
+        overflow-y: visible;
+    }
+    .note {
+        width: 163px;
+        height: 163px;
+        padding: 10px 10px 10px 10px;
+        position: absolute;
+        overflow-y: scroll;
+    }
+
+    .x {
+        position: absolute;
+        color: white;
+        top: 0px;
+        right: -20px;
+        width: 25px;
+        height: 25px;
     }
 </style>
